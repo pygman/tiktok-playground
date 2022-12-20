39f9e03b3c0a5ab100b148dcf91057ceb512fd03
pygman <pygman@aliyun.com>
2022-12-20 12:35:58 +0800

    fix card multi select

--- assets/scripts/CardManageScene.ts
+++ assets/scripts/CardManageScene.ts
@@ -7,6 +7,7 @@ const {ccclass, property} = _decorator;
 @ccclass('CardManage')
 export class CardManage extends Component {
     private selected: number = -1;
+    private mergeSet: Set<number> = new Set<number>();
     private content: cc.Node;
     private client: Client;
 
@@ -90,6 +91,10 @@ export class CardManage extends Component {
                 .getComponent(cc.Label).string = 'Tok';
         });
          */
+        this.node.getChildByName('MergeCard').on(Input.EventType.TOUCH_END, (_) => {
+            console.log(`MergeCard clicked`);
+            this.cardMerge();
+        });
         this.node.getChildByName('CardsBoard').on(Input.EventType.TOUCH_START, (_) => {
             console.log(`CardsBoard clicked`);
             if (this.selected !== -1) {
@@ -147,106 +152,6 @@ export class CardManage extends Component {
             }
         }
 
-        const cardMerge = (i: number) => {
-            if (this.selected === -1) {
-                const msgBoxNode = cc.instantiate(this.msgBoxPrefab);
-                msgBoxNode.name = `MsgBox`;
-                msgBoxNode.parent = this.node;
-                msgBoxNode.getChildByName('msg').getComponent(cc.Label).string = `Please left click to choose a card first`;
-                msgBoxNode.setPosition(0, 500);
-                cc.tween(msgBoxNode)
-                    .to(0.5, { position: new cc.Vec3(0, 300, 0) }, { easing: 'elasticOut' })
-                    .delay(3)
-                    .to(0.5, { position: new cc.Vec3(0, 500, 0) }, { easing: 'elasticIn' })
-                    .start();
-                setTimeout(() => {
-                    msgBoxNode.destroy();
-                }, 8000);
-                return;
-            }
-            if (this.selected === i) {
-                const msgBoxNode = cc.instantiate(this.msgBoxPrefab);
-                msgBoxNode.name = `MsgBox`;
-                msgBoxNode.parent = this.node;
-                msgBoxNode.getChildByName('msg').getComponent(cc.Label).string = `Can not merge to itself`;
-                msgBoxNode.setPosition(0, 500);
-                cc.tween(msgBoxNode)
-                    .to(0.5, { position: new cc.Vec3(0, 300, 0) }, { easing: 'elasticOut' })
-                    .delay(3)
-                    .to(0.5, { position: new cc.Vec3(0, 500, 0) }, { easing: 'elasticIn' })
-                    .start();
-                setTimeout(() => {
-                    msgBoxNode.destroy();
-                }, 8000);
-                return;
-            } else {
-                console.log(`do merge ${this.selected} ${i}`);
-                const cardLeft = this.cards[this.selected];
-                const cardLeftNode = cc.instantiate(this.cardPrefab)
-                cardLeftNode.name = `CardLeft`;
-                cardLeftNode.parent = this.node;
-                cardLeftNode.setPosition(-720, 0);
-                if (cardLeft.texture) {
-                    cc.loader.load({ url: cardLeft.texture, type: 'png' }, (err, texture) => {
-                        if (err) return;
-                        cardLeftNode.getChildByName('pic').getComponent(cc.Sprite).spriteFrame = cc.SpriteFrame.createWithImage(texture as cc.ImageAsset);
-                    });
-                }
-                cardLeftNode.getChildByName('title').getComponent(cc.Label).string = cardLeft.name;
-                cardLeftNode.getChildByName('desc').getComponent(cc.Label).string =
-                    `  ${cardLeft.rarity}\nlevel: ${cardLeft.level}\nweapon: ${cardLeft.weapon}\nskill: ${cardLeft.skill}\nrace: ${cardLeft.race}\ntribe: ${cardLeft.tribe}\n`;
-
-                const cardRight = this.cards[i];
-                const cardRightNode = cc.instantiate(this.cardPrefab)
-                cardRightNode.name = `CardRight`;
-                cardRightNode.parent = this.node;
-                cardRightNode.setPosition(720, 0);
-                if (cardRight.texture) {
-                    cc.loader.load({ url: cardRight.texture, type: 'png' }, (err, texture) => {
-                        if (err) return;
-                        cardRightNode.getChildByName('pic').getComponent(cc.Sprite).spriteFrame = cc.SpriteFrame.createWithImage(texture as cc.ImageAsset);
-                    });
-                }
-                cardRightNode.getChildByName('title').getComponent(cc.Label).string = cardRight.name;
-                cardRightNode.getChildByName('desc').getComponent(cc.Label).string =
-                    `  ${cardRight.rarity}\nlevel: ${cardRight.level}\nweapon: ${cardRight.weapon}\nskill: ${cardRight.skill}\nrace: ${cardRight.race}\ntribe: ${cardRight.tribe}\n`;
-
-                cc.tween(cardLeftNode)
-                    .to(1.2, { position: new cc.Vec3(-60, 0, 0) }, { easing: 'elasticOut' })
-                    .start();
-                cc.tween(cardRightNode)
-                    .to(1.2, { position: new cc.Vec3(60, 0, 0) }, { easing: 'elasticOut' })
-                    .start();
-
-                setTimeout(() => {
-                    const bombNode = cc.instantiate(this.bombPrefab);
-                    bombNode.name = 'Bomb';
-                    bombNode.parent = this.node;
-                    bombNode.setPosition(0, 0);
-                    bombNode.getComponent(cc.Animation).play('bomb');
-                }, 800);
-                setTimeout(() => {
-                    this.node.getChildByName('CardLeft').destroy();
-                    this.node.getChildByName('CardRight').destroy();
-                    this.node.getChildByName('Bomb').destroy();
-                }, 2000);
-                // this.client.merge_cards(this.cards[this.selected], this.cards[i])
-                const msgBoxNode = cc.instantiate(this.msgBoxPrefab);
-                msgBoxNode.name = `MsgBox`;
-                msgBoxNode.parent = this.node;
-                msgBoxNode.getChildByName('msg').getComponent(cc.Label).string = `merged cards hash: \n 0x0`;
-                msgBoxNode.setPosition(0, 500);
-                cc.tween(msgBoxNode)
-                    .to(0.5, { position: new cc.Vec3(0, 300, 0) }, { easing: 'elasticOut' })
-                    .delay(3)
-                    .to(0.5, { position: new cc.Vec3(0, 500, 0) }, { easing: 'elasticIn' })
-                    .start();
-                setTimeout(() => {
-                    msgBoxNode.destroy();
-                }, 8000);
-            }
-        }
-
         cards.forEach((card, i) => {
             const cardNode = cc.instantiate(this.cardPrefab)
             cardNode.name = `Card${i}`;
@@ -270,9 +175,79 @@ export class CardManage extends Component {
                 if (event.getButton() === 0) {
                     cardClick(i);
                 } else {
-                    cardMerge(i)
+                    if (this.mergeSet.has(i)) {
+                        this.mergeSet.delete(i);
+                        this.content.getChildByName(`Card${i}`).getComponent(cc.Sprite).color = cc.Color.WHITE;
+                    } else {
+                        this.mergeSet.add(i);
+                        this.content.getChildByName(`Card${i}`).getComponent(cc.Sprite).color = cc.Color.RED;
+                    }
+                    this.node.getChildByName('MergeCard').active = this.mergeSet.size >= 2;
                 }
             });
         })
+    }
+
+    cardMerge () {
+        if (this.mergeSet.size < 2) {
+            return;
+        }
+        let i = 0
+        this.mergeSet.forEach((n) => {
+            console.log(`n: ${n}, i: ${i++}`)
+            const positionX = i % 2 ? -720 : 720;
+            const tweenToX = i % 2 ? -60 : 60;
+            setTimeout(() => {
+                const card = this.cards[n];
+                const cardNode = cc.instantiate(this.cardPrefab)
+                cardNode.name = `CardMerge${n}`;
+                cardNode.parent = this.node;
+                cardNode.setPosition(positionX, 0);
+                if (card.texture) {
+                    cc.loader.load({url: card.texture, type: 'png'}, (err, texture) => {
+                        if (err) return;
+                        cardNode.getChildByName('pic').getComponent(cc.Sprite).spriteFrame = cc.SpriteFrame.createWithImage(texture as cc.ImageAsset);
+                    });
+                }
+                cardNode.getChildByName('title').getComponent(cc.Label).string = card.name;
+                cardNode.getChildByName('desc').getComponent(cc.Label).string =
+                    `  ${card.rarity}\nlevel: ${card.level}\nweapon: ${card.weapon}\nskill: ${card.skill}\nrace: ${card.race}\ntribe: ${card.tribe}\n`;
+
+                cc.tween(cardNode)
+                    .to(1.2, {position: new cc.Vec3(tweenToX, 0, 0)}, {easing: 'elasticOut'})
+                    .start();
+            }, 800 * i);
+        });
+        setTimeout(() => {
+            const bombNode = cc.instantiate(this.bombPrefab);
+            bombNode.name = 'Bomb';
+            bombNode.parent = this.node;
+            bombNode.setPosition(0, 0);
+            bombNode.getComponent(cc.Animation).play('bomb');
+        }, 800 * (this.mergeSet.size + 1));
+        setTimeout(() => {
+            this.mergeSet.forEach((n) => {
+                this.node.getChildByName(`CardMerge${n}`).destroy();
+                this.content.getChildByName(`Card${n}`).getComponent(cc.Sprite).color = cc.Color.WHITE;
+            });
+            this.node.getChildByName('Bomb').destroy();
+            this.mergeSet.clear();
+            this.node.getChildByName('MergeCard').active = this.mergeSet.size >= 2;
+        }, 800 * (this.mergeSet.size + 1) + 1200);
+
+        // this.client.merge_cards(this.cards.filter((card, i) => this.mergeSet.has(i));
+        const msgBoxNode = cc.instantiate(this.msgBoxPrefab);
+        msgBoxNode.name = `MsgBox`;
+        msgBoxNode.parent = this.node;
+        msgBoxNode.getChildByName('msg').getComponent(cc.Label).string = `merged cards hash: \n 0x0`;
+        msgBoxNode.setPosition(0, 500);
+        cc.tween(msgBoxNode)
+            .to(0.5, {position: new cc.Vec3(0, 300, 0)}, {easing: 'elasticOut'})
+            .delay(3)
+            .to(0.5, {position: new cc.Vec3(0, 500, 0)}, {easing: 'elasticIn'})
+            .start();
+        setTimeout(() => {
+            msgBoxNode.destroy();
+        }, 8000);
     }
 }
